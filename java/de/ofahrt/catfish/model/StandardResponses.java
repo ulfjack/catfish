8e3a92977e9e1e02f81d3cb362c12b426b938f33
Ulf Adams <ulfjack@google.com>
2018-07-10 14:37:17 +0200

    Add standard responses for all 4XX codes

--- java/de/ofahrt/catfish/model/StandardResponses.java
+++ java/de/ofahrt/catfish/model/StandardResponses.java
@@ -10,11 +10,25 @@ public interface StandardResponses {
 
   // 400
   public static final HttpResponse BAD_REQUEST = new PreconstructedResponse(HttpStatusCode.BAD_REQUEST);
-  public static final HttpResponse NOT_FOUND = new PreconstructedResponse(HttpStatusCode.NOT_FOUND);
+  public static final HttpResponse UNAUTHORIZED = new PreconstructedResponse(HttpStatusCode.UNAUTHORIZED); // 401
+  public static final HttpResponse PAYMENT_REQUIRED = new PreconstructedResponse(HttpStatusCode.PAYMENT_REQUIRED); // 402
+  public static final HttpResponse FORBIDDEN = new PreconstructedResponse(HttpStatusCode.FORBIDDEN); // 403
+  public static final HttpResponse NOT_FOUND = new PreconstructedResponse(HttpStatusCode.NOT_FOUND); // 404
   // TODO: According to the spec, a list of allowed methods must be provided.
   public static final HttpResponse METHOD_NOT_ALLOWED = new PreconstructedResponse(HttpStatusCode.METHOD_NOT_ALLOWED); // 405
+  public static final HttpResponse NOT_ACCEPTABLE = new PreconstructedResponse(HttpStatusCode.NOT_ACCEPTABLE); // 406
+  public static final HttpResponse PROXY_AUTH_REQUIRED = new PreconstructedResponse(HttpStatusCode.PROXY_AUTH_REQUIRED); // 407
+  public static final HttpResponse REQUEST_TIMEOUT = new PreconstructedResponse(HttpStatusCode.REQUEST_TIMEOUT); // 408
+  public static final HttpResponse CONFLICT = new PreconstructedResponse(HttpStatusCode.CONFLICT); // 409
+  public static final HttpResponse GONE = new PreconstructedResponse(HttpStatusCode.GONE); // 410
+  public static final HttpResponse LENGTH_REQUIRED = new PreconstructedResponse(HttpStatusCode.LENGTH_REQUIRED); // 411
+  public static final HttpResponse PRECONDITION_FAILED = new PreconstructedResponse(HttpStatusCode.PRECONDITION_FAILED); // 412
+  public static final HttpResponse PAYLOAD_TOO_LARGE = new PreconstructedResponse(HttpStatusCode.PAYLOAD_TOO_LARGE); // 413
+  public static final HttpResponse URI_TOO_LONG = new PreconstructedResponse(HttpStatusCode.URI_TOO_LONG); // 414
   public static final HttpResponse UNSUPPORTED_MEDIA_TYPE = new PreconstructedResponse(HttpStatusCode.UNSUPPORTED_MEDIA_TYPE); // 415
+  public static final HttpResponse RANGE_NOT_SATISFIABLE = new PreconstructedResponse(HttpStatusCode.RANGE_NOT_SATISFIABLE); // 416
   public static final HttpResponse EXPECTATION_FAILED = new PreconstructedResponse(HttpStatusCode.EXPECTATION_FAILED); // 417
+  public static final HttpResponse UPGRADE_REQUIRED = new PreconstructedResponse(HttpStatusCode.UPGRADE_REQUIRED); // 426
 
   // 500
   public static final HttpResponse INTERNAL_SERVER_ERROR = new PreconstructedResponse(HttpStatusCode.INTERNAL_SERVER_ERROR); // 500
