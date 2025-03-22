dbcb5adbf470f181f61e106b0dc01ae1a8b4011c
Ulf Adams <ulf@engflow.com>
2025-03-22 15:23:04 +0100

    Tests: properly close client connections

--- javatest/de/ofahrt/catfish/integration/LocalCatfishServer.java
+++ javatest/de/ofahrt/catfish/integration/LocalCatfishServer.java
@@ -1,7 +1,5 @@
 package de.ofahrt.catfish.integration;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import de.ofahrt.catfish.CatfishHttpServer;
 import de.ofahrt.catfish.TestServlet;
 import de.ofahrt.catfish.bridge.ServletHttpHandler;
@@ -14,6 +12,8 @@ import de.ofahrt.catfish.model.network.NetworkEventListener;
 import de.ofahrt.catfish.model.server.BasicHttpHandler;
 import de.ofahrt.catfish.model.server.ResponsePolicy;
 import de.ofahrt.catfish.model.server.UploadPolicy;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 final class LocalCatfishServer implements Server {
 
@@ -25,39 +25,31 @@ final class LocalCatfishServer implements Server {
   public static final String HTTP_ROOT = "http://localhost:" + HTTP_PORT;
   public static final String HTTPS_ROOT = "https://localhost:" + HTTPS_PORT;
 
-  private final CatfishHttpServer server = new CatfishHttpServer(new NetworkEventListener() {
-    @Override
-    public void shutdown() {
-      if (DEBUG) System.out.println("[CATFISH] Server stopped.");
-    }
+  private final CatfishHttpServer server =
+      new CatfishHttpServer(
+          new NetworkEventListener() {
+            @Override
+            public void shutdown() {
+              if (DEBUG) System.out.println("[CATFISH] Server stopped.");
+            }
 
-    @Override
-    public void portOpened(int port, boolean ssl) {
-      if (DEBUG) System.out.println("[CATFISH] Opening socket on port " + port + (ssl ? " (ssl)" : ""));
-    }
+            @Override
+            public void portOpened(int port, boolean ssl) {
+              if (DEBUG)
+                System.out.println(
+                    "[CATFISH] Opening socket on port " + port + (ssl ? " (ssl)" : ""));
+            }
 
-    @Override
-    public void notifyInternalError(Connection id, Throwable throwable) {
-      throwable.printStackTrace();
-    }
-  });
-
-  private final Thread shutdownHook = new Thread() {
-    @Override
-    public void run() {
-      try {
-        server.stop();
-      } catch (InterruptedException e) {
-        throw new RuntimeException(e);
-      }
-    }
-  };
+            @Override
+            public void notifyInternalError(Connection id, Throwable throwable) {
+              throwable.printStackTrace();
+            }
+          });
 
   private boolean startSsl;
   private UploadPolicy uploadPolicy = UploadPolicy.DENY;
 
-  public LocalCatfishServer() throws IOException {
-  }
+  public LocalCatfishServer() throws IOException {}
 
   public LocalCatfishServer setUploadPolicy(UploadPolicy uploadPolicy) {
     this.uploadPolicy = uploadPolicy;
@@ -71,11 +63,12 @@ final class LocalCatfishServer implements Server {
 
   @Override
   public void start() throws Exception {
-    ServletHttpHandler handler = new ServletHttpHandler.Builder()
-        .withSessionManager(new SessionManager())
-        .exact("/compression.html", new TestServlet())
-        .directory("/", new HttpRequestTestServlet())
-        .build();
+    ServletHttpHandler handler =
+        new ServletHttpHandler.Builder()
+            .withSessionManager(new SessionManager())
+            .exact("/compression.html", new TestServlet())
+            .directory("/", new HttpRequestTestServlet())
+            .build();
 
     server.addHttpHost(
         "localhost",
@@ -87,12 +80,10 @@ final class LocalCatfishServer implements Server {
     if (startSsl) {
       server.listenHttps(HTTPS_PORT);
     }
-    Runtime.getRuntime().addShutdownHook(shutdownHook);
   }
 
   @Override
   public void shutdown() throws Exception {
-    Runtime.getRuntime().removeShutdownHook(shutdownHook);
     server.stop();
   }
 
@@ -107,21 +98,22 @@ final class LocalCatfishServer implements Server {
     return data.replace("\n", "\r\n").getBytes("ISO-8859-1");
   }
 
-  private HttpResponse send(String hostname, int port, boolean ssl, byte[] content) throws IOException {
+  private HttpResponse send(String hostname, int port, boolean ssl, byte[] content)
+      throws IOException {
     if (ssl && !startSsl) {
       throw new IllegalStateException();
     }
-    HttpConnection connection = HttpConnection.connect(
-        hostname, port, ssl ? TestHelper.getSSLContext() : null);
-    connection.write(content);
-    HttpResponse response = connection.readResponse();
-    connection.close();
-    return response;
+    try (HttpConnection connection =
+        HttpConnection.connect(hostname, port, ssl ? TestHelper.getSSLContext() : null)) {
+      connection.write(content);
+      return connection.readResponse();
+    }
   }
 
   @Override
   public HttpResponse sendSsl(String content) throws IOException {
-    return send(LocalCatfishServer.HTTP_SERVER, LocalCatfishServer.HTTPS_PORT, true, toBytes(content));
+    return send(
+        LocalCatfishServer.HTTP_SERVER, LocalCatfishServer.HTTPS_PORT, true, toBytes(content));
   }
 
   @Override
@@ -140,8 +132,6 @@ final class LocalCatfishServer implements Server {
       throw new IllegalStateException();
     }
     return HttpConnection.connect(
-        HTTP_SERVER,
-        ssl ? HTTPS_PORT : HTTP_PORT,
-        ssl ? TestHelper.getSSLContext() : null);
+        HTTP_SERVER, ssl ? HTTPS_PORT : HTTP_PORT, ssl ? TestHelper.getSSLContext() : null);
   }
 }
