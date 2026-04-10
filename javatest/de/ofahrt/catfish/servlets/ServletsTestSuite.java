a1509da44c92ff201e624e21032d616366424b79
Ulf Adams <ulf@engflow.com>
2026-04-10 18:06:29 +0200

    Remove the old CheckCompression Servlet

--- javatest/de/ofahrt/catfish/servlets/ServletsTestSuite.java
+++ javatest/de/ofahrt/catfish/servlets/ServletsTestSuite.java
@@ -6,7 +6,6 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-  CheckCompressionTest.class,
   CheckPostTest.class,
   DirectoryServletTest.class,
   RedirectServletTest.class,
