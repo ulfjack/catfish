b9df5d1571d80a3140ef26fa6a1dbc823bd715db
Ulf Adams <ulf@engflow.com>
2026-04-17 14:35:52 +0200

    Avoid regex parsing

--- java/de/ofahrt/catfish/upload/UrlEncodedParser.java
+++ java/de/ofahrt/catfish/upload/UrlEncodedParser.java
@@ -4,11 +4,8 @@ import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class UrlEncodedParser {
-  private static final Pattern QUERY_PART_PATTERN = Pattern.compile("([^&=]*)=([^&]*)");
 
   public FormDataBody parse(byte[] input) {
     return parse(input, 0, input.length);
@@ -17,10 +14,13 @@ public class UrlEncodedParser {
   public FormDataBody parse(byte[] input, int offset, int length) {
     List<FormEntry> entries = new ArrayList<>();
     String inputAsString = new String(input, offset, length, StandardCharsets.UTF_8);
-    Matcher mq = QUERY_PART_PATTERN.matcher(inputAsString);
-    while (mq.find()) {
-      String name = URLDecoder.decode(mq.group(1), StandardCharsets.UTF_8);
-      String value = URLDecoder.decode(mq.group(2), StandardCharsets.UTF_8);
+    for (String pair : inputAsString.split("&", -1)) {
+      int eq = pair.indexOf('=');
+      if (eq < 0) {
+        continue;
+      }
+      String name = URLDecoder.decode(pair.substring(0, eq), StandardCharsets.UTF_8);
+      String value = URLDecoder.decode(pair.substring(eq + 1), StandardCharsets.UTF_8);
       entries.add(new FormEntry(name, value));
     }
     return new FormDataBody(entries);
