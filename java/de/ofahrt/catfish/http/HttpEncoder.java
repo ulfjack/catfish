ab627c4d69f828b88c326d3b866877c29a9c8f55
Ulf Adams <ulf@engflow.com>
2026-04-24 11:32:37 +0200

    Remove dead code

--- java/de/ofahrt/catfish/http/HttpEncoder.java
+++ java/de/ofahrt/catfish/http/HttpEncoder.java
@@ -1,6 +1,5 @@
 package de.ofahrt.catfish.http;
 
-import de.ofahrt.catfish.model.HttpHeaders;
 import de.ofahrt.catfish.model.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
@@ -30,20 +29,6 @@ public final class HttpEncoder {
     buffer.append(response.getStatusMessage());
     buffer.append(CRLF);
     Iterator<Map.Entry<String, String>> it = response.getHeaders().iterator();
-    while (it.hasNext()) {
-      Map.Entry<String, String> entry = it.next();
-      buffer.append(entry.getKey());
-      buffer.append(": ");
-      buffer.append(entry.getValue());
-      buffer.append(CRLF);
-    }
-    buffer.append(CRLF);
-    return buffer.toString().getBytes(StandardCharsets.UTF_8);
-  }
-
-  public static byte[] headersToByteArray(HttpHeaders headers) {
-    StringBuilder buffer = new StringBuilder(200);
-    Iterator<Map.Entry<String, String>> it = headers.iterator();
     while (it.hasNext()) {
       Map.Entry<String, String> entry = it.next();
       buffer.append(entry.getKey());
